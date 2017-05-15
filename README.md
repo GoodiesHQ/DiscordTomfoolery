b44c75382d23007cf099d7a865551d07e2cebfdf
GoodiesHQ <goodies@protonmail.com>
2017-05-14 21:47:15 -0700

    Added webhook_spammer.py

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DiscordTomfoolery
 A collection of various discord-related tools.
+
+
+## webhook_spammer.py
+Sends multiple HTTP post requests to a webhook URL that will be able to send messages to the designated channel. It does have the ability to mention `@everyone`. This implies that it may have high amount of permissions, so some bots that perform guild-specific checks may actually be able to kick/ban members, although this has not been tested.
+
+#### Help
+```python
+Usage: webhooker.py [-h] --webhook WEBHOOK --name NAME --avatar-url AVATAR_URL
+                    --message MESSAGE [--count COUNT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --webhook WEBHOOK, -w WEBHOOK
+                        The Webhook URL
+  --name NAME, -n NAME  The name you want to appear on the Bot
+  --avatar-url AVATAR_URL, -a AVATAR_URL
+                        The avatar image file
+  --message MESSAGE, -m MESSAGE
+                        The content of the message
+  --count COUNT, -c COUNT
+                        The number of messages to spam.
+```
---
+++ webhook_spammer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+import asyncio
+import aiohttp
+import json
+
+class WebHooker:
+    _headers = {"Content-Type": "application/json"}
+
+    def __init__(self, webhook:str, username:str, avatar_url:str, content:str, count:int, loop:asyncio.AbstractEventLoop=None):
+        self.__loop = loop or asyncio.get_event_loop()
+        self.__webhook = webhook
+        self.__count = count
+        self.__sem = asyncio.BoundedSemaphore(1)  # maybe support multiple webhooks later?
+        self.__data = json.dumps(dict(avatar_url=avatar_url, content=content, username=username))
+
+    async def __aenter__(self, *args,  **kwargs):
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        """Might do something later"""
+        return True
+
+    async def _send(self, session):
+        async with self.__sem:
+            try:
+                async with session.post(self.__webhook, headers=self._headers, data=self.__data) as resp:
+                    x = await resp.text()
+                    print(x)
+            except Exception as e:
+                print(e)
+            finally:
+                await asyncio.sleep(0.35)
+
+    async def spam(self):
+        print("Spamming...")
+        async with aiohttp.ClientSession(loop=self.__loop) as session:
+            tasks = [self._send(session) for _ in range(self.__count)]
+            await asyncio.gather(*tasks, loop=self.__loop)
+
+async def main():
+    ap = ArgumentParser()
+    ap.add_argument("--webhook", "-w", type=str, required=True, help="The Webhook URL")
+    ap.add_argument("--name", "-n", type=str, required=True, help="The name you want to appear on the Bot")
+    ap.add_argument("--avatar-url", "-a", type=str, required=True, help="The avatar image file")
+    ap.add_argument("--message", "-m", type=str, required=True, help="The content of the message")
+    ap.add_argument("--count", "-c", type=int, default=10, help="The number of messages to spam.")
+    args = ap.parse_args()
+    async with WebHooker(args.webhook, args.name, args.avatar_url, args.message, args.count) as wh:
+        await wh.spam()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.close()
