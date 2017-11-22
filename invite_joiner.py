93fc6f0e8d78eeb0b8947fdb2df63d4aca88a319
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-11-22 21:01:14 +0000

    Update invite_joiner.py
    Your code has been rated at 9.25/10 (previous run: 6.41/10, +2.84)
    need i say more

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,42 +1,69 @@
 #!/usr/bin/env python3
+"""
+A collection of various discord-related tools.
+"""
+
 from argparse import ArgumentParser, ArgumentError
-import aiohttp
-import asyncio
 import re
+import asyncio
+import aiohttp
+
 
 def create_headers(token):
+    "Template for headers"
+
     return {
         "authorization": token,
         "X-Context-Properties": token,
     }
 
 def readfile(filename):
-    with open(filename, "r") as fin:
-        return [line.strip() for line in fin]
+    "Reads a file and returns file.readlines equivalent output."
+
+    with open(filename) as fin:
+        return list(map(str.strip, fin))
 
 def inv_to_api(invite):
+    "Changes an abstracted invite link into its API constituent"
+
     code = re.search(r"https://discord.gg/(\S*)", invite)
     if not code:
         raise ArgumentError
-    return "https://discordapp.com/api/v6/invite/{}".format(code.groups(1)[0])
+    return "https://discordapp.com/api/v6/invite%s" % code.groups(1)[0]
 
 async def join_server(api_url, token, sem):
+    "Joins a Discord server"
+
     try:
         async with sem:
             async with aiohttp.ClientSession() as session:
-                async with session.post(api_url, headers=create_headers(token), timeout=3.0) as resp:
+                async with session.post(api_url, headers=create_headers(token), timeout=3) as resp:
                     if resp.status != 200:
-                        print("Failure: ({}) {}".format(resp.status, api_url))
+                        print("Failure: (%s) %s" % (resp.status, api_url))
                         print(await resp.text())
-    except Exception as e:
-        print(e)
+    except Exception as exc:
+        print(exc)
 
 async def main():
-    ap = ArgumentParser()
-    ap.add_argument("--tokens", "-t", required=True, type=str, help="Filename containing a list of okens")
-    ap.add_argument("--invite", "-i", required=True, type=inv_to_api, help="The discord invite link code.")
-    ap.add_argument("--connections", "-c", type=int, default=50, help="Number of concurrent connections")
-    args = ap.parse_args()
+    parser = ArgumentParser()
+    parser.add_argument("--tokens", "-t",
+                        required=True,
+                        type=str,
+                        help="Filename containing a list of okens")
+
+    parser.add_argument("--invite",
+                        "-i",
+                        required=True,
+                        type=inv_to_api,
+                        help="The discord invite link code.")
+
+    parser.add_argument("--connections",
+                        "-c",
+                        type=int,
+                        default=50,
+                        help="Number of concurrent connections")
+
+    args = parser.parse_args()
     tokens = readfile(args.tokens)
     sem = asyncio.BoundedSemaphore(args.connections)
     tasks = [join_server(args.invite, token, sem) for token in tokens]
